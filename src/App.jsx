daa5746133f90ffbd7cba37cdee8ccf57cbb3fbb
Leandro Atallah <leandroatallah@gmail.com>
2020-02-26 13:17:04 -0300

    Add: Start project and done first story

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+export default function App() {
+    return (
+        <>
+            <div id="form-container">
+                <h1>Border Radius Previewer</h1>
+                <div id="border-container">
+                    <div id="border-radius-element"></div>
+                </div>
+            </div>
+        </>
+    )
+}
