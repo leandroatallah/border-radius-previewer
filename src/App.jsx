5dc58bf08c0aba5d7f006ad06ca17f7d56d2d660
Leandro Atallah <leandroatallah@gmail.com>
2020-02-29 17:18:43 -0300

    Update: 8D Adjustment

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './App.css'
 
 export default function App() {
     const [eightDimension, setEightDimensions] = useState(false)
     const [topLeft, setTopLeft] = useState(0)
+    const [topLeftAlt, setTopLeftAlt] = useState(0)
     const [topRight, setTopRight] = useState(0)
+    const [topRightAlt, setTopRightAlt] = useState(0)
     const [bottomRight, setBottomRight] = useState(0)
+    const [bottomRightAlt, setBottomRightAlt] = useState(0)
     const [bottomLeft, setBottomLeft] = useState(0)
+    const [bottomLeftAlt, setBottomLeftAlt] = useState(0)
+
+    useEffect(() => {
+        const borderElem = document.getElementById('border-radius-element')
+
+        if(eightDimension==='true') {
+            borderElem.style.borderRadius = `${topLeft}px ${topRight}px ${bottomRight}px ${bottomLeft}px / ${topLeftAlt}px ${topRightAlt}px ${bottomRightAlt}px ${bottomLeftAlt}px`
+        } else {
+            borderElem.style.borderRadius = `${topLeft}px ${topRight}px ${bottomRight}px ${bottomLeft}px`
+        }
+    })
 
     function getBorderRadius() {
-        if(eightDimension) {
-            console.log('true')
-            return `${topLeft}px ${topRight}px ${bottomRight}px ${bottomLeft}px / ${topLeft}px ${topRight}px ${bottomRight}px ${bottomLeft}px`
+        if(eightDimension==='true') {
+            return `${topLeft}px ${topRight}px ${bottomRight}px ${bottomLeft}px / ${topLeftAlt}px ${topRightAlt}px ${bottomRightAlt}px ${bottomLeftAlt}px`
         } else {
-            console.log('false')
             return `${topLeft}px ${topRight}px ${bottomRight}px ${bottomLeft}px`
         }
     }
@@ -41,7 +53,7 @@ export default function App() {
                 <h1>Border Radius Previewer</h1>
 
                 <div id="border-container">
-                    <div id="border-radius-element" style={{'borderRadius': getBorderRadius()}}>
+                    <div id="border-radius-element">
                         <div id="style-result">
                             <div>border-radius: {getBorderRadius()}</div>
                             <div>-webkit-border-radius: {getBorderRadius()}</div>
@@ -58,9 +70,13 @@ export default function App() {
                     </div>
 
                     <input type="number" id="top-left" placeholder="0" min="0" max="1000" onChange={(e) => setTopLeft(e.target.value)} />
+                    <input type="number" id="top-left-alt" style={{display: eightDimension==='true' ? 'block' : 'none'}} placeholder="0" min="0" max="1000" onChange={(e) => setTopLeftAlt(e.target.value)} />
                     <input type="number" id="top-right" placeholder="0" min="0" max="1000" onChange={(e) => setTopRight(e.target.value)} />
+                    <input type="number" id="top-right-alt" style={{display: eightDimension==='true' ? 'block' : 'none'}} placeholder="0" min="0" max="1000" onChange={(e) => setTopRightAlt(e.target.value)} />
                     <input type="number" id="bottom-right" placeholder="0" min="0" max="1000" onChange={(e) => setBottomRight(e.target.value)} />
+                    <input type="number" id="bottom-right-alt" style={{display: eightDimension==='true' ? 'block' : 'none'}} placeholder="0" min="0" max="1000" onChange={(e) => setBottomRightAlt(e.target.value)} />
                     <input type="number" id="bottom-left" placeholder="0" min="0" max="1000" onChange={(e) => setBottomLeft(e.target.value)} />
+                    <input type="number" id="bottom-left-alt" style={{display: eightDimension==='true' ? 'block' : 'none'}} placeholder="0" min="0" max="1000" onChange={(e) => setBottomLeftAlt(e.target.value)} />
                 </div>
             </div>
         </>
